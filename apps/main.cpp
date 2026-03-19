db6f32622a511a9addb83c7a2d112fb5e3b1a959
mccakit <mccakit09@gmail.com>
2026-03-19 17:02:59 +0300

    refactor

--- apps/main.cpp
+++ apps/main.cpp
@@ -7,23 +7,24 @@ import cppbuild;
 auto main(int argc, char **argv) -> int
 {
     using namespace cppbuild;
-    cxxopts::Options options("cppbuild", "builds and links c and cpp sources");
-    options.add_options()("mode", "Program mode", cxxopts::value<std::string>())(
-        "build_dir", "Build directory", cxxopts::value<std::string>())(
-        "src_dir", "Source directory", cxxopts::value<std::string>())(
-        "toolchain", "Toolchain file", cxxopts::value<std::string>())(
-        "prefix", "Install prefix", cxxopts::value<std::string>())(
-        "compdb_path", "Compilation database path", cxxopts::value<std::string>());
+    cxxopts::Options options{"cppbuild", "C++ build system"};
+    auto opts = options.add_options();
+    opts("mode", "Program mode", cxxopts::value<std::string>());
+    opts("build_dir", "Build directory", cxxopts::value<std::string>());
+    opts("src_dir", "Source directory", cxxopts::value<std::string>());
+    opts("toolchain", "Toolchain file", cxxopts::value<std::string>());
+    opts("prefix", "Install prefix", cxxopts::value<std::string>());
+    opts("compdb_path", "Compilation database path", cxxopts::value<std::string>());
     cxxopts::ParseResult result = options.parse(argc, argv);
     std::filesystem::path self_path = std::filesystem::weakly_canonical(argv[0]);
     std::string mode = result["mode"].as<std::string>();
     if (mode == "configure")
     {
         return configure({.src_dir = result["src_dir"].as<std::string>(),
-                               .build_dir = result["build_dir"].as<std::string>(),
-                               .toolchain_path = result["toolchain"].as<std::string>(),
-                               .self_path = self_path,
-                               .prefix = std::filesystem::weakly_canonical(result["prefix"].as<std::string>())});
+                          .build_dir = result["build_dir"].as<std::string>(),
+                          .toolchain_path = result["toolchain"].as<std::string>(),
+                          .self_path = self_path,
+                          .prefix = std::filesystem::weakly_canonical(result["prefix"].as<std::string>())});
     }
     else if (mode == "scan")
     {
